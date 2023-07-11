ebde190bbfcbc1e27ba0f64886d7628a462cff47
Martin Buder <132884932+martinBuder@users.noreply.github.com>
2023-07-11 19:25:06 +0200

    added cards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StartSiteComponent } from './start-site/start-site.component';
+import { GameComponent } from './game/game.component';
 
 const routes: Routes = [
   { path: '', component: StartSiteComponent},
+  { path: 'game', component: GameComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartSiteComponent } from './start-site/start-site.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartSiteComponent
+    StartSiteComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,

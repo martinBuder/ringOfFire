efe690c5e5674105c8c82000e25c7d16240dd2c6
Martin Buder <132884932+martinBuder@users.noreply.github.com>
2023-07-12 08:08:19 +0200

    added random cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
   pickCardAnimation = false;
+  currentCard: string = '';
+  game: Game = new Game(); 
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+    
+  }
   
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = `${this.game.stack.pop()}`;
+      this.pickCardAnimation = true;
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
   }
 }
---
+++ src/models/game.ts
@@ -0,0 +1,17 @@
+export class Game {
+	public players: string[] = [];
+	public stack: string[] = [];
+	public playCard: string[] = [];
+	public currentPlayer: number = 0;
+
+	constructor() {
+		for (let i = 1; i < 14; i++) {
+			this.stack.push(`ace_${i}`); 
+			this.stack.push(`clubs_${i}`); 
+			this.stack.push(`diamonds_${i}`); 
+			this.stack.push(`hearts_${i}`); 
+			this.stack.sort(() => Math.random() - 0.5); //zufällige Sortierung
+		}
+	}
+
+}
